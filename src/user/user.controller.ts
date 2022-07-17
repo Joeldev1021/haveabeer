4151fc75b49765eb4d5521f6b87ba74840b0c838
brahianpdev <zk.spywn@gmail.com>
2022-07-17 10:28:54 -0300

    feat: add user controller

---
+++ src/user/user.controller.ts
@@ -0,0 +1,88 @@
+// External modules
+import { Request, Response } from 'express';
+
+// Internal modules
+import { CreateUserDto } from './dtos/create.user.dto';
+import { UpdateUserDto } from './dtos/update.user.dto';
+import HttpStatus from '../config/http.status';
+import userService from './user.service';
+
+class UserController {
+    constructor() { }
+
+    async findAll(req: Request, res: Response) {
+        try {
+            const users = await userService.findAll();
+
+            return res.status(HttpStatus.OK).send(users);
+        } catch (error) {
+            res.status(HttpStatus.INTERNAL_SERVER_ERROR);
+        }
+    }
+
+    async findById(req: Request, res: Response) {
+        try {
+            const user = await userService.findById(req.params.id);
+
+            return res.status(HttpStatus.OK).send(user);
+        } catch (error) {
+            res.status(HttpStatus.INTERNAL_SERVER_ERROR);
+        }
+    }
+
+    async findByEmail(req: Request, res: Response) {
+        try {
+            const user = await userService.findByEmail(req.params.email);
+
+            return res.status(HttpStatus.OK).send(user);
+        } catch (error) {
+            res.status(HttpStatus.INTERNAL_SERVER_ERROR);
+        }
+    }
+
+    async findTopDrunks(req: Request, res: Response) {
+        try {
+            const drunks = await userService.findTopDrunks();
+
+            return res.status(HttpStatus.OK).send(drunks);
+        } catch (error) {
+            res.status(HttpStatus.INTERNAL_SERVER_ERROR);
+        }
+    }
+
+    async create(req: Request, res: Response) {
+        try {
+            const createUserDto: CreateUserDto = req.body;
+
+            const user = await userService.create(createUserDto);
+
+            return res.status(HttpStatus.OK).send(user);
+        } catch (error) {
+            res.status(HttpStatus.INTERNAL_SERVER_ERROR);
+        }
+    }
+
+    async update(req: Request, res: Response) {
+        try {
+            const updateUserDto: UpdateUserDto = req.body;
+
+            const user = await userService.update(req.params.id, updateUserDto);
+
+            return res.status(HttpStatus.OK).send(user);
+        } catch (error) {
+            res.status(HttpStatus.INTERNAL_SERVER_ERROR);
+        }
+    }
+
+    async delete(req: Request, res: Response) {
+        try {
+            const user = await userService.delete(req.params.id);
+
+            return res.status(HttpStatus.OK).send('User deleted');
+        } catch (error) {
+            res.status(HttpStatus.INTERNAL_SERVER_ERROR);
+        }
+    }
+}
+
+export default new UserController();
