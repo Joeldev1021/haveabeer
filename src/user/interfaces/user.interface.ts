413a9653333598f07084c5c23623cd520e922567
brahianpdev <zk.spywn@gmail.com>
2022-07-17 10:29:53 -0300

    feat: add user interface

---
+++ src/user/interfaces/user.interface.ts
@@ -0,0 +1,14 @@
+
+export interface IUser {
+    // Personal
+    firstName: string;
+    lastName: string;
+    email: string;
+    password: string;
+    creditCard: string;
+
+    // Profile
+    username: string;
+    description: string;
+    donations: number;
+}
